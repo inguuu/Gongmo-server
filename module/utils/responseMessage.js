b0ca2f9285e8e494c7f38677c75f09215f634761
inguuu <dlsrb8949@gmail.com>
2018-03-13 17:09:44 +0900

    commit response@3

--- module/utils/responseMessage.js
+++ module/utils/responseMessage.js
@@ -19,13 +19,5 @@ module.exports = {
   DUPLICATED_ID_FAIL:"중복된 ID가 있습니다.",
   NOT_CORRECT_USERINFO:"ID가 존재하지 않거나 비밀번호가 일치하지 않습니다.",
 
-  NOT_FOUND_BOARDID: "ID와 일치하는 게시물이 없습니다.",
-  BOARD_SELECT_SUCCESS: "게시물 조회 성공",
-  BOARD_SELECT_FAIL: "게시물 조회 실패",
-  BOARD_INSERT_SUCCESS: "게시물 삽입 성공",
-  BOARD_INSERT_FAIL: "게시물 삽입 실패",
-  BOARD_DELETE_SUCCESS: "게시물 삭제 성공",
-  BOARD_DELETE_FAIL: "게시물 삭제 실패",
-  BOARD_PASSWORD_ERROR: "게시물 비밀번호가 다릅니다.",
-  ENTER_ALL:"제목, 내용, 게시물 비밀번호, 작성자를 모두 입력하세요"
+
 };
